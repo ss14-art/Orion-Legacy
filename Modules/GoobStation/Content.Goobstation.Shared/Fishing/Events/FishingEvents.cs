4fdfff60af34626c6908ad2aa1843b0935c336cd
Rouden <149893554+Roudenn@users.noreply.github.com>
2026-05-28 21:27:10 +0300

    Revert "Fishing (#33)" (#61)
    This reverts commit ecc34e1f4342caf8343b6d0cd59d09161f819aab.

--- Modules/GoobStation/Content.Goobstation.Shared/Fishing/Events/FishingEvents.cs
+++
@@ -1,7 +0,0 @@
-using Content.Shared.Actions;
-
-namespace Content.Goobstation.Shared.Fishing.Events;
-
-public sealed partial class ThrowFishingLureActionEvent : WorldTargetActionEvent;
-
-public sealed partial class PullFishingLureActionEvent : InstantActionEvent;
